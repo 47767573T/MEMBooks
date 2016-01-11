17d1e7e6ed8451db1f291c4311d709e17d2496e9
MVDam <47767573t@iespoblenou.org>
2016-01-11 22:48:42 +0100

    Base de proyecto

---
+++ MEMBooks/app/src/main/java/app/membooks/A_MainFragment.java
@@ -0,0 +1,22 @@
+package app.membooks;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class A_MainFragment extends Fragment {
+
+    public A_MainFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.a_main_lay, container, false);
+    }
+}
